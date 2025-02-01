251ec42517444bd46e484a28e623fc7bc20600f9
Sadeeq <sadeeqndanusa@gmail.com>
2025-02-01 18:57:27 +0100

    Add sticky header functionality and transition effect

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const helo = document.querySelector('.menu-phone')
 const nav = document.querySelector('.nav-lists')
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
